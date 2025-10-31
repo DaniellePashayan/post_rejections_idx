f8eb073337f1bfda995583d71b8d6542c1028e64
Danielle Pashayan <daniellepashayan@gmail.com>
2025-10-31 11:55:16 -0400

    updated improts

--- pages/pp_batch.py
+++ pages/pp_batch.py
@@ -1,3 +1,10 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+import time
+
 class PaymentPostingBatch:
     BATCH_HEADER = (By.CLASS_NAME, "fe_c_tabs__label-text")
     BATCH_GROUP_TEXT = (By.ID, "formHeader")
